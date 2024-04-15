5fff5aca456242b6a957578edae7f2883daf8abf
filizhelvaci <filizhelvaci82@gmail.com>
2024-04-15 15:39:04 +0300

    Changed

---
+++ src/main/java/com/flz/DevOpsPipelineApp.java
@@ -0,0 +1,13 @@
+package com.flz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsPipelineApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsPipelineApp.class, args);
+    }
+
+}
